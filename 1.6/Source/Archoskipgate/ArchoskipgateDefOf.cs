1f51b75b12b94d03d4469625056cae869f5dfb62
Kieran David Evans <keyz182@gmail.com>
2025-06-20 05:57:49 +0100

    Cookie cutter initial commit

---
+++ 1.6/Source/Archoskipgate/ArchoskipgateDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Archoskipgate;
+
+[DefOf]
+public static class ArchoskipgateDefOf
+{
+    // Remember to annotate any Defs that require a DLC as needed e.g.
+    // [MayRequireBiotech]
+    // public static GeneDef YourPrefix_YourGeneDefName;
+    
+    static ArchoskipgateDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(ArchoskipgateDefOf));
+}
---
+++ 1.6/Source/Archoskipgate/ArchoskipgateMod.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Archoskipgate;
+
+public class ArchoskipgateMod : Mod
+{
+    public static Settings settings;
+
+    public ArchoskipgateMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from Archoskipgate");
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.Archoskipgate.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "Archoskipgate_SettingsCategory".Translate();
+    }
+}
